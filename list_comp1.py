c1c19d320fd6e15fe1598c9f27234b17a3032bb8
Muhammad Habib Khan <92048010+MuhammadHabibKhan@users.noreply.github.com>
2023-02-22 17:17:32 +0500

    Delete list_comp1.py

--- list_comp1.py
+++
@@ -1,6 +0,0 @@
-# (i)Write a list comprehension which, from a list, generates a lowercased version of each string that has 
-# length greater than five
-
-old_list = ['TENET', 'OPPENHEIMER', 'INTERSTELLAR', 'CARS']
-new_list = [movies.lower() for movies in old_list if len(movies) > 5]
-print(new_list)
